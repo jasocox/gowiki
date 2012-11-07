553e1c2af987946a98ea53105c309a30a49569fd
Jason Cox <JasonWilburCox@gmail.com>
2012-11-07 09:52:37 +0000

    Basic server

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
 func main() {
-  fmt.Println("Hello World")
+  fmt.Println("Starting Server...")
+
+  http.ListenAndServe(":8080", nil)
 }
