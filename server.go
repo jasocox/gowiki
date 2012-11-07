7dd43cf08dcda77624306eb1061c36de0811240a
Jason Cox <JasonWilburCox@gmail.com>
2012-11-07 10:50:43 +0000

    Made a basic view for the main page

--- server.go
+++ server.go
@@ -3,14 +3,25 @@ package main
 import (
   "fmt"
   "net/http"
+  "html/template"
 )
 
 type Server struct {}
 
+// Wiki View
+const views = "view/"
+const mainView = "main.html"
+
+var templates = template.Must(template.ParseFiles(views + mainView))
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
+  err := templates.ExecuteTemplate(w, mainView, nil)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
 
+// Server
 func main() {
   s := new(Server)
   fmt.Println("Starting Server...")
