da2d59dd1b8fcc2c3f3ed606d8e27bf4670c1e17
Jason Cox <JasonWilburCox@gmail.com>
2012-11-14 20:54:41 -0500

    Listing wikis to the cli based on files in the wiki dir

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -2,11 +2,15 @@ package wiki
 
 import (
   "fmt"
+  "regexp"
+  "log"
   "os"
   "errors"
 )
 
 const pages = "pages/"
+var validWikiFile = regexp.MustCompile("^[^/.]+[.]txt$")
+var wikiTitleRegexp = regexp.MustCompile("[^.]+")
 
 type GoWiki struct { }
 
@@ -22,24 +26,41 @@ func init() {
   }
 
   if !fi.IsDir() {
-    panic(errors.New("Wiki storage location is not a directory: " + pages))
+    log.Fatal(errors.New("Wiki storage location is not a directory: " + pages))
   }
 }
 
-func (gw GoWiki) PageList() ([]WikiPage, error) {
+func (gw GoWiki) PageList() (wp []WikiPage, err error) {
   pageDir, err := os.Open(pages)
   if err != nil {
-    return nil, err
+    return
   }
 
   wikiList, err := pageDir.Readdirnames(0)
   if err != nil {
-    return nil, err
+    return
   }
 
   for page := range wikiList {
-    fmt.Println("Wiki page file: " + pages + wikiList[page])
+    title, err := pageTitle(wikiList[page])
+    if err != nil {
+      log.Println(err)
+      continue
+    }
+    fmt.Println("Wiki page title: " + title)
+  }
+  wp = []WikiPage{WikiPage{"page1", ""}, WikiPage{"page2", ""}}
+
+  return wp, nil
+}
+
+func pageTitle(file string) (name string, err error) {
+  if validWikiFile.MatchString(file) {
+    name = wikiTitleRegexp.FindString(file)
+    fmt.Println("name: " + name)
+  } else {
+    err = errors.New("Invalid wiki: " + file)
   }
 
-  return []WikiPage{WikiPage{"page1", ""}, WikiPage{"page2", ""}}, nil
+  return
 }
