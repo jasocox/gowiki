d72e45f2a39921dc7b76d325659560189557ad77
Jason Cox <JasonWilburCox@gmail.com>
2012-11-21 14:49:39 -0500

    Refactored all of the file read/write code to a data file

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -1,20 +1,5 @@
 package wiki
 
-import (
-  "strings"
-  "regexp"
-  "log"
-  "os"
-  "io/ioutil"
-  "errors"
-)
-
-const pages = "pages/"
-var (
-  validWikiFile = regexp.MustCompile("^[^/.]+[.]txt$")
-  wikiTitleRegexp = regexp.MustCompile("[^.]+")
-)
-
 type GoWiki struct { }
 
 type WikiPage struct {
@@ -22,59 +7,14 @@ type WikiPage struct {
   Body string
 }
 
-func init() {
-  fi, err := os.Stat(pages)
-  if err != nil {
-    panic(err)
-  }
-
-  if !fi.IsDir() {
-    log.Fatal(errors.New("Wiki storage location is not a directory: " + pages))
-  }
-}
-
 func (gw GoWiki) PageList() (wp []WikiPage, err error) {
-  wikiList, err := ioutil.ReadDir(pages)
-  if err != nil {
-    return
-  }
-
-  for page := range wikiList {
-    title, err := pageTitle(wikiList[page].Name())
-    if err != nil {
-      log.Println(err)
-      continue
-    }
-    wp = append(wp, WikiPage{title, ""})
-  }
-
-  return wp, nil
+  return getPageList()
 }
 
-func (gw GoWiki) GetWiki(title string) (wp WikiPage, err error) {
-  body, err := ioutil.ReadFile(pages + title + ".txt")
-  wp.Title = title
-  wp.Body = strings.TrimSpace(string(body))
-
-  return
+func (gw GoWiki) GetWiki(title string) (WikiPage, error) {
+  return getWiki(title)
 }
 
-func (gw GoWiki) CreateWiki(title string, body string) (wp WikiPage, err error) {
-  log.Println("New wiki body: " + body)
-  wp.Title = title
-  wp.Body = strings.TrimSpace(body)
-
-  err = ioutil.WriteFile(pages + title + ".txt", []byte(body), 0600)
-
-  return
-}
-
-func pageTitle(file string) (name string, err error) {
-  if validWikiFile.MatchString(file) {
-    name = wikiTitleRegexp.FindString(file)
-  } else {
-    err = errors.New("Invalid wiki: " + file)
-  }
-
-  return
+func (gw GoWiki) CreateWiki(title string, body string) (WikiPage, error) {
+  return createWiki(title, body)
 }
